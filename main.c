cb0c44f5951ce025cba3a8e06856d1887b493a2d
BrahimCaparros <brahim.caparros@epitech.eu>
2023-04-02 20:52:08 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 char *my_putstr(char *str)
 {
     int i = 0;
-    while (str[i] != '\0') {
-        my_putchar(str[i]);
+    while (str[i] != '\0')
+    {
+        write(1, &str[i], 1);
         i++;
     }
     return (str);
